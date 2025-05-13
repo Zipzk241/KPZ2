899bcb690761869b3d59ae43d57fe824774b95b9
Zipzk241 <zipzk241_nap@student.ztu.edu.ua>
2025-05-13 19:05:53 +0300

    Create Constants

---
+++ src/main/java/com/kpz2/util/Constants.java
@@ -0,0 +1,9 @@
+package com.kpz2.util;
+
+public class Constants {
+    public static final int BOARD_SIZE = 3;
+    public static final String PLAYER_X_SYMBOL = "X";
+    public static final String PLAYER_O_SYMBOL = "O";
+    public static final String EMPTY_CELL = "";
+    public static final String HISTORY_FILE = "games_history.csv";
+}
